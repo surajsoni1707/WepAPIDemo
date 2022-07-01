45156df547df1525e697c8b5df960dea10232cb9
surajsoni1707 <105903958+surajsoni1707@users.noreply.github.com>
2022-07-01 14:27:48 +0530

    commit

--- Model/Product.cs
+++ Model/Product.cs
@@ -14,7 +14,7 @@ namespace WepAPIDemo.Model
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
 
     }
 }
